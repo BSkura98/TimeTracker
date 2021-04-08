be5f92a1952185733223cf0c095276d6a91824cc
215877 <q89nTi10>
2021-04-08 18:44:11 +0200

    Added saving timers in localstorage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Form from "./components/Form";
+import TimerList from "./components/TimerList";
 
 function App() {
+  const [timerName, setTimerName] = useState("");
+  const [timers, setTimers] = useState([]);
+
+  useEffect(() => {
+    let data = localStorage.getItem("timers");
+    setTimers(JSON.parse(data));
+    console.log(data);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("timers", JSON.stringify(timers));
+  }, [timers]);
+
   return (
     <div className="App">
       <header>
         <h1>Time Tracker</h1>
       </header>
-      <Form />
+      <Form
+        timers={timers}
+        setTimers={setTimers}
+        timerName={timerName}
+        setTimerName={setTimerName}
+      />
+      <TimerList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({ timerName, setTimerName, timers, setTimers }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setTimerName(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTimers([
+      ...timers,
+      { name: timerName, time: 0, id: new Date().getTime().toString() },
+    ]);
+    setTimerName("");
+  };
+
   return (
-    <form>
-      <input type="text" className="timer-input" />
+    <form onSubmit={submitHandler}>
+      <input
+        value={timerName}
+        onChange={inputTextHandler}
+        type="text"
+        className="timer-input"
+      />
       <button className="add-timer-button" type="submit">
         <i className="fas fa-plus-circle"></i>
       </button>
