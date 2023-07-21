13c1e2bd7e2656b11a80bad71f1f3a4f824e07e3
Bartłomiej Skura <bartek33s@wp.pl>
2023-07-21 15:43:14 +0200

    Remove old timer context

--- src/App.js
+++ src/App.js
@@ -1,89 +1,26 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 
 import "./App.scss";
 import Form from "./components/Form/Form";
 import TimerList from "./components/TimerList/TimerList";
-import { timersReducer } from "./redux/reducers/timersReducer";
 import { ClockIcon } from "./icons";
-import { getTimers, setCurrentTimer } from "./redux/actions/timer";
 import { store } from "./redux/store";
 
-export const TimerContext = React.createContext();
-
-const defaultState = {
-  timers: [],
-  currentTimer: { id: 0 },
-  updatedSecond: 0,
-  updatedMinute: 0,
-  updatedHour: 0,
-};
-
 function App() {
-  const [interv, setInterv] = useState();
-  const [state, dispatch] = useReducer(timersReducer, defaultState);
-
-  useEffect(() => {
-    dispatch(getTimers());
-  }, []);
-
-  const incrementTimer = () => {
-    dispatch(incrementTimer());
-  };
-
-  const startStopTimer = (timer, timerRemoved) => {
-    if (timerRemoved === true) {
-      if (timer.id === state.currentTimer.id) {
-        stopTimer();
-
-        dispatch(setCurrentTimer({ id: 0 }));
-      }
-      return;
-    }
-
-    if (timer.id === state.currentTimer.id) {
-      stopTimer();
-
-      dispatch(setCurrentTimer({ id: 0 }));
-    } else if (timer.id === 0) {
-      startTimer();
-
-      dispatch(setCurrentTimer(timer));
-    } else {
-      dispatch(setCurrentTimer(timer));
-
-      stopTimer();
-      startTimer();
-    }
-  };
-
-  const startTimer = () => {
-    setInterv(
-      setInterval(() => {
-        incrementTimer();
-      }, 1000)
-    );
-  };
-
-  const stopTimer = () => {
-    clearInterval(interv);
-  };
-
   return (
-    <TimerContext.Provider value={{ startStopTimer, state, dispatch }}>
-      <ReduxProvider store={store}>
-        <div className="App">
-          <header>
-            <div className="logo fs-2 mb-3">
-              <ClockIcon />
-              <h1 className="title">Time Tracker</h1>
-            </div>
-          </header>
-          <Form />
-          <TimerList />
-        </div>
-      </ReduxProvider>
-    </TimerContext.Provider>
+    <ReduxProvider store={store}>
+      <div className="App">
+        <header>
+          <div className="logo fs-2 mb-3">
+            <ClockIcon />
+            <h1 className="title">Time Tracker</h1>
+          </div>
+        </header>
+        <Form />
+        <TimerList />
+      </div>
+    </ReduxProvider>
   );
 }
 
