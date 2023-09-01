3e4a708bb4cd9c71d3c859437a101aff423d06e5
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-09-01 09:54:53 +0200

    Save timers created in single timers page in database (#21)
    * Add play/stop button to form in simple timers page
    
    * Save timers created in single timers page in database

--- src/pages/Timers/Form/Form.js
+++ src/pages/Timers/Form/Form.js
@@ -1,26 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BootstrapForm from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { useMutation } from "@apollo/client";
 
 import "./style.scss";
-import { addTimer } from "../../../redux/slices/timers";
-import { setCurrentPageDate } from "../../../redux/slices/advancedTimers";
+import {
+  setCurrentPageDate,
+  setCurrentTimer,
+  setFormTimerName,
+} from "../../../redux/slices/advancedTimers";
+import { PlayIcon, StopIcon } from "../../../icons";
+import { formatTime } from "../../../helpers/formatTime";
+import {
+  CREATE_AND_START_TIMER_ENTRY,
+  STOP_TIMER_ENTRY,
+} from "../../../graphql/mutations";
+import { GET_TIMERS_ENTRIES } from "../../../graphql/queries";
 
 const Form = () => {
   const dispatch = useDispatch();
-  const [timerName, setTimerName] = useState("");
   const state = useSelector((state) => state.advancedTimers);
+  const [stopTimerEntry, { error: stopTimerEntryError }] = useMutation(
+    STOP_TIMER_ENTRY,
+    { refetchQueries: [GET_TIMERS_ENTRIES] }
+  );
+  const [createAndStartTimerEntry, { error: createAndStartTimerEntryError }] =
+    useMutation(CREATE_AND_START_TIMER_ENTRY);
 
   const handleAddTimerInput = (e) => {
-    setTimerName(e.target.value);
+    dispatch(setFormTimerName(e.target.value));
   };
 
-  const handleSubmit = (e) => {
+  const handleStartTimer = async (e) => {
     e.preventDefault();
-    dispatch(addTimer(timerName));
-    setTimerName("");
+    const result = await createAndStartTimerEntry({
+      variables: {
+        startTime: new Date(),
+        timerName: state.formTimerName,
+      },
+    });
+
+    if (createAndStartTimerEntryError) {
+      console.log(createAndStartTimerEntryError);
+    } else {
+      dispatch(setCurrentTimer(result.data.createTimerEntry));
+    }
+  };
+
+  const handleStopTimer = (e) => {
+    e.preventDefault();
+    stopTimerEntry({
+      variables: {
+        id: state.currentTimer.id,
+        endTime: new Date(),
+      },
+    });
+
+    if (stopTimerEntryError) {
+      console.log(stopTimerEntryError);
+    } else {
+      dispatch(setFormTimerName(""));
+      dispatch(setCurrentTimer(null));
+    }
   };
 
   const handleChangeDate = (e) => {
@@ -31,16 +75,30 @@ const Form = () => {
   return (
     <div className="add-timer-form-container">
       <Container fluid="md">
-        <Stack className="add-timer-form-stack" direction="horizontal" gap={5}>
-          <form onSubmit={handleSubmit}>
+        <Stack className="add-timer-form-stack" direction="horizontal" gap={1}>
+          <form
+            onSubmit={state.currentTimer ? handleStopTimer : handleStartTimer}
+          >
             <BootstrapForm.Control
-              value={timerName}
+              value={state.formTimerName}
               onChange={handleAddTimerInput}
               type="text"
               className="timer-input me-auto"
-              placeholder="Add timer"
+              placeholder="Timer"
             />
+            <Button type="submit" variant="light">
+              {state.currentTimer ? <StopIcon /> : <PlayIcon />}
+            </Button>
           </form>
+          <div className="current-timer-time">
+            {state.currentTimer
+              ? formatTime(
+                  state.currentTimerTime.hour,
+                  state.currentTimerTime.minute,
+                  state.currentTimerTime.second
+                )
+              : ""}
+          </div>
           <BootstrapForm.Control
             type="date"
             className="timers-date"
